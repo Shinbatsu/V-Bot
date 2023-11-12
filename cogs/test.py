81107a43fdcda07ecaf6509603edaab31f1f1778
Shinbatsu <nojic132@gmail.com>
2023-11-12 18:54:06 +0300

    new api tricks

--- cogs/test.py
+++ cogs/test.py
@@ -1,35 +1,21 @@
-""""
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 6.1.0
-"""
 import discord
 from discord import app_commands
 import datetime
 from discord.ext import commands
 from discord.ext.commands import Context
 
+# class MyModal(discord.ui.Modal):
+#     def __init__(self, *args, **kwargs) -> None:
+#         super().__init__(*args, **kwargs)
 
-class MyView(discord.ui.View):  # Create a class called MyView that subclasses discord.ui.View
-    @discord.ui.button(label="Click me!", style=discord.ButtonStyle.primary)
-    async def button_callback(self, interaction, button):
-        await interaction.response.send_message("You clicked the button!")  # Send a message when the button is clicked
+#         self.add_item(discord.ui.InputText(label="Short Input"))
+#         self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
 
-
-class MyModal(discord.ui.Modal):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-        self.add_item(discord.ui.InputText(label="Short Input"))
-        self.add_item(discord.ui.InputText(label="Long Input", style=discord.InputTextStyle.long))
-
-    async def callback(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Modal Results")
-        embed.add_field(name="Short Input", value=self.children[0].value)
-        embed.add_field(name="Long Input", value=self.children[1].value)
-        await interaction.response.send_message(embeds=[embed])
+#     async def callback(self, interaction: discord.Interaction):
+#         embed = discord.Embed(title="Modal Results")
+#         embed.add_field(name="Short Input", value=self.children[0].value)
+#         embed.add_field(name="Long Input", value=self.children[1].value)
+#         await interaction.response.send_message(embeds=[embed])
 
 
 # Here we name the cog and create a new class for the cog.
@@ -37,24 +23,55 @@ class Testing(commands.Cog, name="testing"):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.hybrid_command()
-    async def modal_slash(self, ctx: Context):
-        """Shows an example of a modal dialog being invoked from a slash command."""
-        modal = MyModal(title="Modal via Slash Command")
-        await ctx.send_modal(modal)
+    @commands.command()
+    async def sql(self, context: Context):
+        await context.defer()
+        user_id=383943093310980096
+        room_id=711201810110742550
+        room =  await self.bot.database.add_user_room(user_id, room_id)
+        embed = discord.Embed(
+            description=f"**{user_id}** add into the database by **{context.author}**, your personal room is {room}",
+            color=0xBEBEFE,
+        )
+        await context.send(embed=embed)
 
-    @commands.hybrid_command(name="button", description="This is a testing command that does nothing.", with_app_command=True)
-    async def button(self, ctx):
-        await ctx.send("Wow! It works!", view=MyView())  # Send a message with our View class that contains the button
+    @commands.command()
+    async def sql2(self, context: Context):
+        await context.defer()
+        user_id=383943093310980096
+        total = await self.bot.database.add_warn(
+            user_id, context.guild.id, context.author.id, "Idiot"
+        )
+        embed = discord.Embed(
+            description=f"**{context.author.id}** was warned by **{context.author}**!\nTotal warns for this user: {total}",
+            color=0xBEBEFE,
+        )
+        embed.add_field(name="Reason:", value="Idiot")
+        await context.send(embed=embed)
+        await context.send(
+                f"{context.author.id}, you were warned by **{context.author}**!\nReason:"
+            )
+    # @commands.Cog.listener()
+    # async def on_message_edit(self, before, after):
+    #     embed=discord.Embed(title="Message Edited", color=0x00FFFF)
+    #     embed.add_field(name='Before:', value=before.content + "u200b", inline=False)
+    #     embed.add_field(name="After:", value=after.content + "u200b", inline=False)
+    #     channel = self.bot.get_channel(self.bot.config["LOG_CHANNEL_ID"])
+    #     await channel.send("DADADAD")
 
-    # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
-    @commands.hybrid_command(name="add", description="This is a testing command that does nothing.", with_app_command=True)
-    async def add(self, ctx, first: int, second: int):
-        # you can use them as they were actual integers
-        sum = first + second
-        await ctx.reply(f"The sum of {first} and {second} is {sum}.")
-
-
+    # @commands.Cog.listener()
+    # async def on_message_delete(self, message):
+    #     channel = self.bot.get_channel(self.bot.config["LOG_CHANNEL_ID"])
+    #     self.bot.logger.info(f"On message delete")
+    #     if not message.author.bot:
+    #         embed = discord.Embed(
+    #             title="Message Deleted",
+    #             description=f"{message.author.mention} deleted a message in {message.channel.mention}",
+    #             color=discord.Color.red(),
+    #         )
+    #         embed.add_field(name="Content", value=message.content, inline=False)
+    #         embed.set_footer(text=f"User ID: {message.author.id} | Deleted at {discord.utils.utcnow()}")
+    #         await channel.send(embed=embed)
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
     await bot.add_cog(Testing(bot))
