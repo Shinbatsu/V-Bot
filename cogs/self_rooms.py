7bb9a7ec78109b4c984352b095940a0ca06900db
Shinbatsu <nojic132@gmail.com>
2023-11-17 01:03:51 +0300

    update self rooms cog

--- cogs/self_rooms.py
+++ cogs/self_rooms.py
@@ -1,92 +1,221 @@
-import discord
-from discord.ext import commands, tasks
-from discord import ui, PartialEmoji
-from discord.ui import View
+from discord.ext import commands
+from discord import ui, PartialEmoji, ButtonStyle, PermissionOverwrite
+from discord.ui import View, button
+from discord.utils import get
+from .embeds.self_rooms_embed import *
+from .modals.self_rooms_modals import *
+from discord import PermissionOverwrite
 
 
-class SelfRooms(commands.Cog, name="tournaments"):
+class SelfRooms(commands.Cog, name="self_rooms"):
     def __init__(self, bot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        autoroles = [
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Roles⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Agents⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀Platform⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-            "⠀⠀⠀⠀⠀⠀⠀⠀Notification⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-        ]
-        for role in autoroles:
-            self.bot.add_roles(member, discord.utils.get(member.guild.roles, name=role))
+    async def on_ready(self):
+        self.guild = self.bot.get_guild(self.bot.config["GUILD_ID"])
+        self.entry_room = get(
+            self.guild.voice_channels, id=self.bot.config["ENTRY_ROOM_CHANNEL_ID"]
+        )
+        self.category = get(
+            self.guild.categories,
+            id=self.bot.config["ACTIVE_NOW_CATEGORY_ID"],
+        )
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        # Check if user has joined entry room
+        if before.channel != self.entry_room and after.channel == self.entry_room:
+            has_room = await self.bot.database.is_owner(member.id)
+            if not has_room:
+                room_name = f"{member.name}'s room"
+                user_room = await self.guild.create_voice_channel(
+                    room_name, category=self.category, user_limit=5
+                )
+                await self.bot.database.add_user_room(member.id, user_room.id, room_name)
+                await member.edit(voice_channel=user_room)
+                invite_link = await user_room.create_invite(unique=True)
+                await member.send(embed=get_room_link_embed(self.bot, invite_link.url))
+                return
+            else:
+                room_id = await self.bot.database.get_user_room_id(member.id)
+                room = get(self.guild.channels, id=room_id)
+                await member.edit(voice_channel=room)
+                return
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        user_room_id = await self.bot.database.get_user_room_id(member.id)
+        user_room = get(self.guild.voice_channels, id=user_room_id)
+        await self.bot.database.delete_user_room(member.id)
+        await user_room.delete()
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        is_user_channel = await self.bot.database.is_user_channel(channel.id)
+        if is_user_channel:
+            await self.bot.database.delete_user_room(room_id=channel.id)
+        return
 
     @commands.command()
     @commands.has_role("Creator")
-    async def room_settings(self, ctx):
+    async def room_settings_panel(self, ctx):
         if ctx.channel.id != self.bot.config["ROOM_SETTINGS_CHANNEL_ID"]:
             return
-        rooms_settings_embed = discord.Embed(
-            title="Управление приватными каналами",
-            color=discord.Colour.from_rgb(36, 128, 70),
-        )
-        rooms_settings_img = discord.Embed(color=discord.Colour.from_rgb(36, 128, 70))
-        rooms_settings_img.set_image(
-            url="https://cdn.discordapp.com/attachments/1171751170033848360/1173278135119261786/settings_logo.png?ex=65635f69&is=6550ea69&hm=3805968a6ca094a17dae87a60f9c426fce2809ed3ff3db65a30f95f99e3d8675&"
-        )
-        rooms_settings_embed.add_field(name="<:custom_lock:1173271460970758204> - ```Заблокировать вход в канал```", value="", inline=False)
-        rooms_settings_embed.add_field(name="<:kick:1173271463244087336> - ```Исключить пользователя из канала```", value="", inline=False)
-        rooms_settings_embed.add_field(
-            name="<:slots:1173271467060904050> - ```Изменить количество слотов в канале```", value="", inline=False
-        )
-        rooms_settings_embed.add_field(name="<:change_owner:1173271455786602526> - ```Изменить владельца канала```", value="", inline=False)
-        rooms_settings_embed.add_field(name="<:rename:1173271464699498677> - ```Изменить название канала```", value="", inline=False)
-        rooms_settings_embed.add_field(name="CREATE ROOM - ```Создать комнату```", value="", inline=False)
-        rooms_settings_embed.set_footer(text="Примечание: Каждый пользователь может иметь только одну комнату!")
-
         await ctx.message.delete()
-        await ctx.send(embed=rooms_settings_img)
-        await ctx.send(embed=rooms_settings_embed, view=RoomSettings(self.bot))
+        await ctx.send(room_settings_banner)
+        await ctx.send(embed=get_room_settings_embed(self.bot), view=RoomSettingsView(self.bot))
+
+
+class RoomSettingsView(
+    View,
+):
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+        self.cooldown = commands.CooldownMapping.from_cooldown(1, 60, commands.BucketType.member)
+
+    @button(
+        label="",
+        emoji=PartialEmoji.from_str("<:custom_lock:1173271460970758204>"),
+        row=0,
+        style=ButtonStyle.secondary,
+    )
+    async def lock(self, interaction, button):
+        member = interaction.user
+        is_owner = await self.bot.database.is_owner(interaction.user.id)
+        if is_owner:
+            is_closed = await self.bot.database.get_is_closed_room(user_id=interaction.user.id)
+            user_room_id = await self.bot.database.get_user_room_id(interaction.user.id)
+            guild = self.bot.get_guild(self.bot.config["GUILD_ID"])
+            user_room = get(guild.voice_channels, id=user_room_id)
+            if not is_closed:
+                await user_room.edit(
+                    overwrites={
+                        member: PermissionOverwrite(connect=False),
+                        guild.default_role: PermissionOverwrite(connect=False),
+                    }
+                )
+                await interaction.response.send_message(
+                    embed=get_room_closed_embed(self.bot), ephemeral=True
+                )
+                await self.bot.database.update_is_close_user_room(
+                    room_id=user_room_id, is_close=True
+                )
+            else:
+                await user_room.edit(
+                    overwrites={
+                        member: PermissionOverwrite(connect=True),
+                        guild.default_role: PermissionOverwrite(connect=True),
+                    }
+                )
+                await interaction.response.send_message(
+                    embed=get_room_opened_embed(self.bot), ephemeral=True
+                )
+                await self.bot.database.update_is_close_user_room(
+                    room_id=user_room_id, is_close=False
+                )
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
+
+    @button(
+        label="",
+        emoji=PartialEmoji.from_str("<:kick:1173271463244087336>"),
+        row=0,
+        style=ButtonStyle.secondary,
+    )
+    async def kick(self, interaction, button):
+        is_owner = await self.bot.database.is_owner(interaction.user.id)
+        if is_owner:
+            await interaction.response.send_modal(KickModal(self.bot))
+            return
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
+
+    @button(
+        label="",
+        emoji=PartialEmoji.from_str("<:slots:1173271467060904050>"),
+        row=0,
+        style=ButtonStyle.secondary,
+    )
+    async def slots(self, interaction, button):
+        is_owner = await self.bot.database.is_owner(interaction.user.id)
+        if is_owner:
+            await interaction.response.send_modal(ChangeSlotsModal(self.bot))
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
+
+    @button(
+        label="",
+        row=0,
+        emoji=PartialEmoji.from_str("<:change_owner:1173271455786602526>"),
+        style=ButtonStyle.secondary,
+    )
+    async def change_owner(self, interaction, button):
+        is_owner = await self.bot.database.is_owner(interaction.user.id)
+        if is_owner:
+            await interaction.response.send_modal(ChangeOwnerModal(self.bot))
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
+
+    @button(
+        label="",
+        row=0,
+        emoji=PartialEmoji.from_str("<:rename:1173271464699498677>"),
+        style=ButtonStyle.secondary,
+    )
+    async def rename(self, interaction, button):
+        is_owner = await self.bot.database.is_owner(interaction.user.id)
+        if is_owner:
+            await interaction.response.send_modal(RenameRoomModal(self.bot))
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
+
+    @button(label="⠀⠀⠀⠀⠀⠀⠀⠀CREATE ROOM⠀⠀⠀⠀⠀⠀⠀", row=1, style=ButtonStyle.success)
+    async def create_room(self, interaction, button):
+        bucket = self.cooldown.get_bucket(interaction.message)
+        retry = bucket.update_rate_limit()
+        if retry:
+            return await interaction.response.send_message(
+                embed=get_slow_down_embed(self.bot, round(retry, 1)), ephemeral=True
+            )
+        user_id = interaction.user.id
+        has_room = await self.bot.database.is_owner(user_id)
+        if has_room:
+            await interaction.response.send_message(
+                embed=get_you_already_has_room_embed(self.bot), ephemeral=True
+            )
+        else:
+            await interaction.response.send_modal(CreateRoomModal(self.bot, interaction.user.name))
+
+    @ui.button(label="DELETE", row=1, style=ButtonStyle.red)
+    async def delete(self, interaction, button):
+        has_room = await self.bot.database.is_owner(interaction.user.id)
+        if has_room:
+            user_room_id = await self.bot.database.get_user_room_id(interaction.user.id)
+            guild = self.bot.get_guild(self.bot.config["GUILD_ID"])
+            user_room = get(guild.voice_channels, id=user_room_id)
+            await self.bot.database.delete_user_room(user_id=interaction.user.id)
+            await user_room.delete()
+            await interaction.response.send_message(
+                embed=get_deleted_embed(self.bot), ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                embed=get_havent_room_embed(self.bot), ephemeral=True
+            )
 
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
     await bot.add_cog(SelfRooms(bot))
-
-
-class RoomSettings(
-    View,
-):
-    def __init__(self, bot):
-        super().__init__()
-        self.value = None
-        self.bot = bot
-
-    @discord.ui.button(
-        label="", emoji=PartialEmoji.from_str("<:custom_lock:1173271460970758204>"), row=0, style=discord.ButtonStyle.secondary
-    )
-    async def lock(self, interaction, button):
-        await interaction.response.send_message("You pressed me!")
-
-    @discord.ui.button(label="", emoji=PartialEmoji.from_str("<:kick:1173271463244087336>"), row=0, style=discord.ButtonStyle.secondary)
-    async def kick(self, interaction, button):
-        await interaction.response.send_message("You pressed me!")
-
-    @discord.ui.button(label="", emoji=PartialEmoji.from_str("<:slots:1173271467060904050>"), row=0, style=discord.ButtonStyle.secondary)
-    async def slots(self, interaction, button):
-        await interaction.response.send_message("You pressed me!")
-
-    @discord.ui.button(
-        label="", row=0, emoji=PartialEmoji.from_str("<:change_owner:1173271455786602526>"), style=discord.ButtonStyle.secondary
-    )
-    async def owner(self, interaction, button):
-        await interaction.response.send_message("You pressed me!")
-
-    @discord.ui.button(label="", row=0, emoji=PartialEmoji.from_str("<:rename:1173271464699498677>"), style=discord.ButtonStyle.secondary)
-    async def rename(self, interaction, button):
-        await interaction.response.send_message("You pressed me!")
-
-    @discord.ui.button(label="⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀CREATE ROOM⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", row=1, style=discord.ButtonStyle.success)
-    async def create_room(self, interaction, button):
-        await interaction.response.defer()
-        current_guild = interaction.guild
-        category = await current_guild.create_category(f"{interaction.user.name}'s tournament")
-        await current_guild.create_voice_channel(f"Voice", category=category, reason=None, user_limit=5)
