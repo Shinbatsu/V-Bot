d0b384d8affbc2e13909fa6778649c9c33d74be3
Shinbatsu <nojic132@gmail.com>
2023-11-17 01:06:17 +0300

    create user activity and CRUDs for them

--- database/__init__.py
+++ database/__init__.py
@@ -1,17 +1,154 @@
 import aiosqlite
+import asyncio
 
 
 class DatabaseManager:
     def __init__(self, *, connection: aiosqlite.Connection) -> None:
         self.connection = connection
 
-    async def add_warn(self, user_id: int, server_id: int, moderator_id: int, reason: str) -> int:
-        """
-        This function will add a warn to the database.
+    # USERS
+    async def add_user(self, user_id):
+        cursor = await self.connection.execute("INSERT INTO users (id) VALUES (?)", (user_id,))
+        await self.connection.commit()
+        return cursor.lastrowid
 
-        :param user_id: The ID of the user that should be warned.
-        :param reason: The reason why the user should be warned.
-        """
+    async def get_valorant_nickname(self, user_id) -> int:
+        rows = await self.connection.execute(
+            "SELECT valorant_nickname FROM users WHERE id=?",
+            (user_id,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] if result[0] else None
+
+    async def get_user_id_by_nickname(self, nickname) -> int:
+        rows = await self.connection.execute(
+            "SELECT id FROM users WHERE valorant_nickname=?",
+            (nickname,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] if result[0] else None
+
+    async def updata_nick_name(self, user_id, valorant_nickname):
+        await self.connection.execute(
+            "UPDATE users SET valorant_nickname=? WHERE id=?",
+            (valorant_nickname, user_id),
+        )
+        await self.connection.commit()
+
+    async def get_user_by_id(self, user_id):
+        cursor = await self.connection.execute("SELECT * FROM users WHERE id=?", (user_id,))
+        return await cursor.fetchone()
+    
+    async def get_all_user_ids(self):
+        cursor = await self.connection.execute("SELECT id FROM users")
+        rows = await cursor.fetchall()
+        return [row[0] for row in rows]  
+    
+    async def add_user_activity(self, user_id, add_activity):
+        cursor = await self.connection.execute(
+            "UPDATE users SET activity = activity + ? WHERE id = ?", (add_activity, user_id)
+        )
+        await self.connection.commit()
+        return cursor.rowcount  #
+
+    async def change_room_owner(self, room_id, user_id):
+        await self.connection.execute(
+            "UPDATE user_rooms SET user_id=? WHERE room_id=?",
+            (user_id, room_id),
+        )
+        await self.connection.commit()
+
+    async def delete_user(self, user_id):
+        cursor = await self.connection.execute("DELETE FROM users WHERE id=?", (user_id,))
+        await self.connection.commit()
+
+    # USER ROOMS
+    async def add_user_room(self, user_id, room_id, room_name):
+        await self.connection.execute(
+            "INSERT INTO user_rooms (user_id, room_id, room_name) VALUES (?, ?, ?)",
+            (user_id, room_id, room_name),
+        )
+        await self.connection.commit()
+
+    async def get_user_room_id(self, user_id) -> int | None:
+        rows = await self.connection.execute(
+            "SELECT room_id FROM user_rooms WHERE user_id=?",
+            (user_id,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] if result is not None else None
+    async def update_is_close_user_room(self,room_id, is_close: bool):
+        await self.connection.execute(
+            "UPDATE user_rooms SET is_close=? WHERE room_id=?",
+            (is_close, room_id),
+        )
+        await self.connection.commit()
+
+    async def rename_user_room(self, room_id, new_room_name):
+        await self.connection.execute(
+            "UPDATE user_rooms SET room_name=? WHERE room_id=?",
+            (new_room_name, room_id),
+        )
+        await self.connection.commit()
+
+    async def change_room_owner(self, room_id, user_id):
+        await self.connection.execute(
+            "UPDATE user_rooms SET user_id=? WHERE room_id=?",
+            (user_id, room_id),
+        )
+        await self.connection.commit()
+
+    async def delete_user_room(self, user_id=-1, room_id=-1):
+        await self.connection.execute(
+            "DELETE FROM user_rooms WHERE user_id=? OR room_id=?",
+            (
+                user_id,
+                room_id,
+            ),
+        )
+        await self.connection.commit()
+
+    async def is_owner(self, user_id):
+        rows = await self.connection.execute(
+            "SELECT COUNT(*) FROM user_rooms WHERE user_id=?",
+            (user_id,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] > 0
+
+    async def get_room_owner(self, room_id) -> int:
+        rows = await self.connection.execute(
+            "SELECT user_id FROM user_rooms WHERE room_id=?",
+            (room_id,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0]
+
+    async def get_is_closed_room(self, room_id=-1, user_id=-1) -> bool:
+        rows = await self.connection.execute(
+            "SELECT is_closed FROM user_rooms WHERE room_id=? OR user_id=?",
+            (room_id, user_id),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] == 1
+
+    async def is_user_channel(self, room_id) -> bool:
+        rows = await self.connection.execute(
+            "SELECT COUNT(*) FROM user_rooms WHERE room_id=?",
+            (room_id,),
+        )
+        async with rows as cursor:
+            result = await cursor.fetchone()
+            return result[0] > 0
+
+    # USER WARNS
+    async def add_warn(self, user_id: int, server_id: int, moderator_id: int, reason: str) -> int:
         rows = await self.connection.execute(
             "SELECT id FROM warns WHERE user_id=? AND server_id=? ORDER BY id DESC LIMIT 1",
             (
@@ -36,13 +173,6 @@ class DatabaseManager:
             return warn_id
 
     async def remove_warn(self, warn_id: int, user_id: int, server_id: int) -> int:
-        """
-        This function will remove a warn from the database.
-
-        :param warn_id: The ID of the warn.
-        :param user_id: The ID of the user that was warned.
-        :param server_id: The ID of the server where the user has been warned
-        """
         await self.connection.execute(
             "DELETE FROM warns WHERE id=? AND user_id=? AND server_id=?",
             (
@@ -64,13 +194,6 @@ class DatabaseManager:
             return result[0] if result is not None else 0
 
     async def get_warnings(self, user_id: int, server_id: int) -> list:
-        """
-        This function will get all the warnings of a user.
-
-        :param user_id: The ID of the user that should be checked.
-        :param server_id: The ID of the server that should be checked.
-        :return: A list of all the warnings of the user.
-        """
         rows = await self.connection.execute(
             "SELECT user_id, server_id, moderator_id, reason, strftime('%s', created_at), id FROM warns WHERE user_id=? AND server_id=?",
             (
@@ -84,44 +207,3 @@ class DatabaseManager:
             for row in result:
                 result_list.append(row)
             return result_list
-
-    async def add_user_room(self, user_id: int, room_id: int):
-        rows = await self.connection.execute(
-            "SELECT id FROM user_rooms WHERE user_id=? ORDER BY id DESC LIMIT 1",
-            (user_id,),
-        )
-        async with rows as cursor:
-            result = await cursor.fetchone()
-            id = result[0] + 1 if result is not None else 1
-            await self.connection.execute(
-                "INSERT INTO user_rooms(id, user_id, room_id) VALUES (?, ?, ?)",
-                (
-                    id,
-                    user_id,
-                    room_id
-                ),
-            )
-            await self.connection.commit()
-            return room_id
-    async def get_user_room(self,user_id:int):
-        rows = await self.connection.execute(
-            "SELECT user_id, strftime('%s', created_at) FROM user_rooms WHERE user_id=?",
-            (
-                user_id
-            ),
-        )
-        async with rows as cursor:
-            result = await cursor.fetchall()
-            result_list = []
-            for row in result:
-                result_list.append(row)
-            return result_list
-    async def delete_user_room(self,user_id:int):
-        await self.connection.execute(
-            "DELETE FROM user_rooms WHERE user_id=?",
-            (
-                user_id,
-            ),
-        )
-        await self.connection.commit()
-    
--- database/schema.sql
+++ database/schema.sql
@@ -6,15 +6,43 @@ CREATE TABLE IF NOT EXISTS `warns` (
   `reason` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE IF NOT EXISTS `user_ranks` (
-  `id` int(11) NOT NULL,
-  `activity` int(20) NOT NULL,
-  `room_id` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- Users table
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INTEGER PRIMARY KEY,
+  `activity` INTEGER NOT NULL DEFAULT 0,
+  `boosts_count` INTEGER NOT NULL DEFAULT 0,
+  `valorant_nickname` VARCHAR(255) DEFAULT NULL,
+  `valorant_rank` VARCHAR(255) DEFAULT NULL,
+  `send_notifications` BOOLEAN NOT NULL DEFAULT 0,
+  `messages_count` INTEGER NOT NULL DEFAULT 0
 );
+
+-- UserRooms table
 CREATE TABLE IF NOT EXISTS `user_rooms` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(20) NOT NULL,
-  `room_id` varchar(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `id` INTEGER PRIMARY KEY,
+  `user_id` INTEGER,
+  `room_id` INTEGER NOT NULL,
+  `room_name` VARCHAR(255) NOT NULL,
+  `is_closed` BOOLEAN NOT NULL DEFAULT 0,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (`room_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+);
+-- BlockedUsers table
+CREATE TABLE IF NOT EXISTS `blocked_users` (
+  `id` INTEGER PRIMARY KEY,
+  `user_id` INTEGER,
+  `blocked_user_id` INTEGER,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+  FOREIGN KEY (`blocked_user_id`) REFERENCES `users`(`id`)
+);
+-- UserReports table
+CREATE TABLE IF NOT EXISTS `user_reports` (
+  `id` INTEGER PRIMARY KEY,
+  `user_id` INTEGER,
+  `reported_user_id` INTEGER,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+  FOREIGN KEY (`reported_user_id`) REFERENCES `users`(`id`)
 );
