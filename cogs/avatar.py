0293af0b90509c87e0c5a5aa1c5a6c8c4da93d4d
Shinbatsu <nojic132@gmail.com>
2023-11-19 13:37:06 +0300

    server avatar

---
+++ cogs/avatar.py
@@ -0,0 +1,60 @@
+import os
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+
+import asyncio
+import random
+
+from PIL import Image, ImageDraw, ImageOps, ImageFont
+from io import BytesIO
+
+
+def square_to_circle(file):
+    data = BytesIO(file.fp.read())
+    square_img = Image.open(data)
+    mask = Image.new("L", square_img.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + square_img.size, fill=255)
+    circular_img = ImageOps.fit(square_img, mask.size, centering=(0.5, 0.5))
+    circular_img.putalpha(mask)
+    return circular_img
+def add_text(image, username):
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype("src/V.ttf", 80)
+    draw.text((730, 170), username, (255, 255, 255), font=font)
+    # Draw a rounded rectangle
+    draw.rounded_rectangle((714, 572, 1809, 615), fill="orange",  radius=100)
+
+class Avatar(commands.Cog, name="avatar"):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="avatar",
+        with_app_command=True,
+        description="Отображает твой профиль на сервере",
+    )
+    @commands.has_role("Member")
+    async def avatar(self, ctx, user_id=None):
+        if user_id is None:
+            user = ctx.author
+        else:
+            user = self.bot.get_user(int(user_id))
+        # loading background
+        background = Image.open("src/img/background.png")
+        # getting user avatar
+        file = await user.avatar.to_file(filename="avatar.png")
+
+        circular_avatar = square_to_circle(file)
+        circular_avatar = circular_avatar.resize((586, 586), Image.Resampling.LANCZOS)
+        background.paste(circular_avatar, (56, 62), circular_avatar)
+
+        # Save the final image and send it in the chat
+        background.save("src/img/user_profile.png")
+        await ctx.send(file=discord.File("src/img/user_profile.png"))
+
+
+async def setup(bot) -> None:
+    await bot.add_cog(Avatar(bot))
