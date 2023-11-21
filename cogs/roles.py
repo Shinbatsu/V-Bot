d4b5ffece5fe966d09b7d5546d4482a7c94a45a1
Shinbatsu <nojic132@gmail.com>
2023-11-21 22:05:01 +0300

    fix

--- cogs/roles.py
+++ cogs/roles.py
@@ -28,22 +28,37 @@ class Roles(commands.Cog, name="roles"):
     )
     async def _panel_roles(self, context: Context) -> None:
         if context.interaction:
-            context = await Context.from_interaction(context.interaction)
-        print(context.interaction)
-        self.bot.logger.info(type(context))
-        await context.reply("Создание панели...", ephemeral=True)
-        self.bot.logger.info(type(context))
-        await context.reply(file=File("src/banners/roles.png"))
-        await context.reply(file=File("src/banners/valorant_ranking.png"))
-        await context.reply(embed=get_pick_rank_embed(), view=PickRankView(self.bot.database))
-        await context.reply(file=File("src/banners/agent_roles.png"))
-        await context.reply(
-            embed=get_pick_your_agents_embed(), view=PickAgentsView(self.bot.database)
-        )
-        await context.reply(file=File("src/banners/nick_color.png"))
-        await context.reply(
-            embed=get_pick_your_nick_color_embed(), view=PickColorView(self.bot.database)
-        )
+            print("INTERACTION", context.interaction)
+            self.bot.logger.info(type(context))
+            await context.interaction.response.send_message("Создание панели через интеракцию...", ephemeral=True)
+            self.bot.logger.info(type(context))
+            await context.interaction.followup.send(file=File("src/banners/roles.png"))
+            await context.interaction.followup.send(file=File("src/banners/valorant_ranking.png"))
+            await context.interaction.followup.send(embed=get_pick_rank_embed(), view=PickRankView(self.bot.database))
+            await context.interaction.followup.send(file=File("src/banners/agent_roles.png"))
+            await context.interaction.followup.send(
+                embed=get_pick_your_agents_embed(), view=PickAgentsView(self.bot.database)
+            )
+            await context.interaction.followup.send(file=File("src/banners/nick_color.png"))
+            await context.interaction.followup.send(
+                embed=get_pick_your_nick_color_embed(), view=PickColorView(self.bot.database)
+            )
+        else:
+            print("CONTEXT", context.interaction)
+            self.bot.logger.info(type(context))
+            await context.reply("Создание панели через контекст...", ephemeral=True)
+            self.bot.logger.info(type(context))
+            await context.reply(file=File("src/banners/roles.png"))
+            await context.reply(file=File("src/banners/valorant_ranking.png"))
+            await context.reply(embed=get_pick_rank_embed(), view=PickRankView(self.bot.database))
+            await context.reply(file=File("src/banners/agent_roles.png"))
+            await context.reply(
+                embed=get_pick_your_agents_embed(), view=PickAgentsView(self.bot.database)
+            )
+            await context.reply(file=File("src/banners/nick_color.png"))
+            await context.reply(
+                embed=get_pick_your_nick_color_embed(), view=PickColorView(self.bot.database)
+            )
 
     async def setup_hook(self) -> None:
         self.add_view(PickRankView(self.bot.database))
